9b619a225b07396b39338566710e1efb4c6e50e8
Fidzal Adrian <94167324+fdzadr@users.noreply.github.com>
2022-10-29 22:10:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let spesial = document.querySelector("#special-btn");
+
+spesial.addEventListener('click', function(event) {
+    alert("LIHAT BAGIAN JUDUL!!!");
+    document.querySelector("#judul").innerHTML = "Terima Kasih Telah Berkunjung :)"
+})
